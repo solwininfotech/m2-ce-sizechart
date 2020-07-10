dccdd97d1f2a238e0d393f6efcfa55138a835951
solwininfotech <info@solwininfotech.com>
2020-07-10 12:27:39 +0530

    Initial release

---
+++ registration.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Solwin Infotech
+ * Solwin Product Sizechart
+ *
+ * @category   Solwin
+ * @package    Solwin_Sizechart
+ * @copyright  Copyright © 2006-2016 Solwin (https://www.solwininfotech.com)
+ * @license    https://www.solwininfotech.com/magento-extension-license/
+ */
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Solwin_Sizechart',
+    __DIR__
+);
